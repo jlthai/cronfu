4a2c7d31c9229217450efee0cd2566024238c159
Jimmy Thai <jimmy@zephlit.com>
2015-04-29 00:52:41 -0400

    Update deploy config

--- config.rb
+++ config.rb
@@ -24,6 +24,8 @@ end
 activate :deploy do |deploy|
   deploy.build_before = true
   deploy.method = :git
+  deploy.remote = 'git@github.com:jlthai/jlthai.github.io.git'
+  deploy.branch = 'master'
 end
 
 helpers do
